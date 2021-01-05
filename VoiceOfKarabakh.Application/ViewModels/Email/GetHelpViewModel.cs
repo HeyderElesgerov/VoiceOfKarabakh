6e3e2b4f3a918eaa1ac1a143e46a1e21dd23e330
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-01-05 08:53:35 +0400

    Added VM for ToGetHelp page

--- VoiceOfKarabakh.Application/ViewModels/Email/GetHelpViewModel.cs
+++ VoiceOfKarabakh.Application/ViewModels/Email/GetHelpViewModel.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace VoiceOfKarabakh.Application.ViewModels.Email
 {
     public class GetHelpViewModel
     {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required, DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public IEnumerable<IFormFile> Documents { get; set; }
+
+        public string Note { get; set; }
     }
 }
